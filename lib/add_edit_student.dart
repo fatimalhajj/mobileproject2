35637844bc50a1ecce74f11664ea7b246331bea7
batoulfahda <11933875@students.liu.edu.lb>
2024-01-09 11:53:01 +0200

    my code

--- lib/add_edit_student.dart
+++ lib/add_edit_student.dart
@@ -78,7 +78,7 @@ class _Add_Edit_StudentState extends State<Add_Edit_Student> {
                     ),
                     SizedBox(height: 10.0,),
                     MyTextFields(
-                      hintedtext: "ex@gmail.com",
+                      hintedtext: "e@gmail.com",
                       labeltext: "Email",
                       inputcontroller: Email,
                     ),
