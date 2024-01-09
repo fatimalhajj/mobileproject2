66fe4088b52c890771b18747c274c45baa231423
batoulfahda <11933875@students.liu.edu.lb>
2024-01-09 11:25:41 +0200

    my code

---
+++ lib/student_controller.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'model.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+import 'model.dart';
+
+
+class StudentController{
+
+  static const VIEW_URL = "https://green-eyed-mothers.000webhostapp.com/view.php";
+  static const CREATE_URL = "https://green-eyed-mothers.000webhostapp.com/create.php";
+  static const DELETE_URL = "https://green-eyed-mothers.000webhostapp.com/delete.php";
+  static const UPDATE_URL = "https://green-eyed-mothers.000webhostapp.com/update.php";
+
+  List<studentModel> studentFromJson(String jsonstring) {
+    final data = json.decode(jsonstring);
+    return List<studentModel>.from(
+        data.map((item)=> studentModel.fromJson(item)));
+  }
+
+  Future<List<studentModel>> getStudents() async {
+    String view_ip = "https://green-eyed-mothers.000webhostapp.com/view.php";
+    final response = await http.get(Uri.parse(VIEW_URL));
+    if(response.statusCode == 200){
+      List<studentModel> list = studentFromJson(response.body);
+      return list;
+    } else{
+      return <studentModel>[];
+    }
+  }
+
+  Future<String> addStudent(studentModel studentModel) async {
+    final response = await http.post(Uri.parse(CREATE_URL), body: studentModel.toJsonAdd());
+    if(response.statusCode == 200) {
+      return response.body;
+    }else {
+      return "Error";
+    }
+  }
+
+  Future<String> UpdateStudent(studentModel studentModel) async{
+    final response = await http.post(Uri.parse(UPDATE_URL), body: studentModel.toJsonDelete_and_Update());
+    if(response.statusCode == 200) {
+      return response.body;
+    }else {
+      return "Error";
+    }
+  }
+
+  Future<String> DeleteStudent(studentModel studentModel) async{
+    final response = await http.post(Uri.parse(DELETE_URL), body: studentModel.toJsonDelete_and_Update());
+    if(response.statusCode == 200) {
+      return response.body;
+    }else {
+      return "Error";
+    }
+  }
+
+}
